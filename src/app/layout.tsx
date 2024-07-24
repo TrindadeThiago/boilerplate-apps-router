0e22ac0301adc3a1c641f9d9c1c877a17f775c25
Thiago Trindade <thiago.trindadexd@gmail.com>
2024-07-23 21:03:43 -0300

    feat: configure jest styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import StyledComponentsRegistry from '@/lib/registry'
-import { Providers } from '@/providers'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
